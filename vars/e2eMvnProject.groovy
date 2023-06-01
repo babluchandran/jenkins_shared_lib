63553965d05d26619581c1db76b211a7d4887180
babluchandran <mailtobabluchandran@gmail.com>
2023-06-01 11:51:17 +0530

    Update e2eMvnProject.groovy

--- vars/e2eMvnProject.groovy
+++ vars/e2eMvnProject.groovy
@@ -1,57 +1,57 @@
 def call(Map args) {
-  pipeline {
-       agent any
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   sh "mvn --version"
-                   sh "java -version"
-                   sh "echo ${args.name}"
-               }
-           }
-           stage("Checkout Code") {
-               steps {
-                   git branch: 'master',
-                       url: "${args.repoUrl}"
-               }
-           }
-           stage("Maven Build") {
-               steps {
-                   sh "cd demo && mvn clean install -DskipTests"
-               }
-           }
-           stage("Docker Build") {
-              when {
-                expression { args.dockerize == "True" }
-              }             
-             steps{
-               script {               
-                 def dockerize = args.dockerize
-                 sh "docker build -t test ."
-               }
-             }
-           }
-			stage("Docker Build") {
-				steps {
-					input {
-						message "Proceed with Docker build?"
-						parameters {
-							choice(
-								choices: 'Yes\nNo',
-								description: 'Select Yes to proceed or No to abort',
-								name: 'Docker_BUILD_CONFIRMATION'
-							)
-						}
-					}
-					script {
-						if (params.Docker_BUILD_CONFIRMATION == 'Yes') {
-							sh "docker ls" // Replace with your Docker build commands
-						} else {
-							error "Docker build aborted by user"
-						}
-					}
-				}
-			}		   
-       }
-   }
+    pipeline {
+        agent any
+        stages {
+            stage("Tools initialization") {
+                steps {
+                    sh "mvn --version"
+                    sh "java -version"
+                    sh "echo ${args.name}"
+                }
+            }
+            stage("Checkout Code") {
+                steps {
+                    git branch: 'master',
+                        url: "${args.repoUrl}"
+                }
+            }
+            stage("Maven Build") {
+                steps {
+                    sh "cd demo && mvn clean install -DskipTests"
+                }
+            }
+            stage("Docker Build") {
+                when {
+                    expression { args.dockerize == "True" }
+                }
+                steps {
+                    script {
+                        def dockerize = args.dockerize
+                        sh "docker build -t test ."
+                    }
+                }
+            }
+            stage("Docker List") {
+                steps {
+                    input {
+                        message "Proceed with Docker build?"
+                        parameters {
+                            choice(
+                                choices: 'Yes\nNo',
+                                description: 'Select Yes to proceed or No to abort',
+                                name: 'Docker_BUILD_CONFIRMATION'
+                            )
+                        }
+                    }
+                    script {
+                        if (params.Docker_BUILD_CONFIRMATION == 'Yes') {
+                            sh "docker ls" // Replace with your Docker build commands
+                        } else {
+                            error "Docker build aborted by user"
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
